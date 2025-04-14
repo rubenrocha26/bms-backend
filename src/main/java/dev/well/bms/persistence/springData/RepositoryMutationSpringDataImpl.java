4ef5242c673d74262f08c9bfa0d0adae22fbdd9d
Ruben Rocha <1241924@isep.ipp.pt>
2025-04-14 17:46:29 +0100

    updated RepositoryMutationSpringDataImpl to use mapper

--- src/main/java/dev/well/bms/persistence/springData/RepositoryMutationSpringDataImpl.java
+++ src/main/java/dev/well/bms/persistence/springData/RepositoryMutationSpringDataImpl.java
@@ -1,8 +1,7 @@
 package dev.well.bms.persistence.springData;
-
-import dev.well.bms.domain.mutation.IFactoryMutation;
 import dev.well.bms.domain.mutation.Mutation;
 import dev.well.bms.domain.valueObject.MutationId;
+import dev.well.bms.mapper.MapperMutation;
 import dev.well.bms.persistence.dataModel.MutationDataModel;
 import dev.well.bms.repository.IRepositoryMutation;
 import org.springframework.stereotype.Repository;
@@ -12,12 +11,12 @@ import java.util.Optional;
 
 @Repository
 public class RepositoryMutationSpringDataImpl implements IRepositoryMutation {
-    IRepositoryMutationSpringData _repositoryMutationSpringData;
-    IFactoryMutation _mutationFactory;
+    private final IRepositoryMutationSpringData _repositoryMutationSpringData;
+    private final MapperMutation _mapperMutation;
 
-    public RepositoryMutationSpringDataImpl(IRepositoryMutationSpringData repositoryMutationSpringData, IFactoryMutation mutationFactory) {
+    public RepositoryMutationSpringDataImpl(IRepositoryMutationSpringData repositoryMutationSpringData) {
         this._repositoryMutationSpringData = repositoryMutationSpringData;
-        this._mutationFactory = mutationFactory;
+        this._mapperMutation = new MapperMutation();
     }
 
     @Override
@@ -35,7 +34,7 @@ public class RepositoryMutationSpringDataImpl implements IRepositoryMutation {
     public Iterable<Mutation> findAll() {
         List<MutationDataModel> listMutationSaved = this._repositoryMutationSpringData.findAll();
 
-        return MutationDataModel.toDomain(_mutationFactory, listMutationSaved);
+        return _mapperMutation.dataModelToDomain(listMutationSaved);
     }
 
     @Override
@@ -43,7 +42,8 @@ public class RepositoryMutationSpringDataImpl implements IRepositoryMutation {
         Optional<MutationDataModel> optionMutationDataModelSaved = this._repositoryMutationSpringData.findById(id.toString());
 
         if (optionMutationDataModelSaved.isPresent()) {
-            Mutation mutationDomain = MutationDataModel.toDomain(_mutationFactory, optionMutationDataModelSaved.get());
+
+            Mutation mutationDomain = _mapperMutation.dataModelToDomain(optionMutationDataModelSaved.get());
             return Optional.of(mutationDomain);
         }else {
             return Optional.empty();
